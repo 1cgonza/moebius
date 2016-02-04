00cc1c1fce94cbd06307bc235a087d9cc92c7fb3
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-02-04 11:18:37 +0800

    add extended users fields

--- library/admin.php
+++ library/admin.php
@@ -48,3 +48,74 @@ function moebius_custom_admin_footer() {
   return '<span id="footer-thankyou">Developed by <a href="http://juancgonzalez.com" target="_blank">Juan Camilo Gonzalez</a></span>.';
 }
 add_filter( 'admin_footer_text', 'moebius_custom_admin_footer' );
+
+/*====================================
+=            EXTEND USERS            =
+====================================*/
+add_action( 'cmb2_admin_init', 'moebius_register_user_profile_metabox' );
+function moebius_register_user_profile_metabox() {
+  $prefix = '_moebius_user_';
+
+  $cmb_user = new_cmb2_box( array(
+    'id'               => $prefix . 'profile',
+    'title'            => 'Perfil de Usuario',
+    'object_types'     => array( 'user' ), // Tells CMB2 to use user_meta vs post_meta
+    'show_names'       => true,
+    'new_user_section' => 'add-new-user', // where form will show on new user page. 'add-existing-user' is only other valid option.
+  ) );
+
+  $cmb_user->add_field( array(
+    'name'     => 'Informacion Adicional para usuarios de Moebius Animaci&oacute;n',
+    'desc'     => 'Esta informaci&oacute; sera visible en algunas de las paginas, por ejemplo en Quienes Somos.',
+    'id'       => $prefix . 'extra_info_title',
+    'type'     => 'title',
+    'on_front' => false,
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => 'Biografia',
+    'desc' => '',
+    'id'   => $prefix . 'bio',
+    'type' => 'wysiwyg',
+    'options' => array(
+      'textarea_rows' => 10,
+      'teeny' => true,
+      'media_buttons' => false,
+    ),
+  ) );
+
+  $cmb_user->add_field( array(
+    'name' => 'Imagen de Perfil',
+    'desc' => 'La imagen que se publicara en tu perfil.',
+    'id'   => $prefix . 'img',
+    'type' => 'file',
+    'options' => array(
+      'url' => false,
+      'add_upload_file_text' => 'Buscar archivo',
+    ),
+  ) );
+
+  if ( current_user_can('edit_pages') ) {
+    $cmb_user->add_field( array(
+      'name' => 'Rol',
+      'desc' => '',
+      'id'   => $prefix . 'role',
+      'type' => 'select',
+      'show_option_none' => true,
+      'options' => array(
+        'principal'   => 'Equipo Principal',
+        'colaborador' => 'Colaborador',
+      ),
+    ) );
+
+    $cmb_user->add_field( array(
+      'name' => 'Titulo del Rol',
+      'desc' => '',
+      'id'   => $prefix . 'role_title',
+      'type' => 'text',
+    ) );
+  }
+
+}
+
+/*=====  End of EXTEND USERS  ======*/
