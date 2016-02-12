8403455bdab041b8c68c48e510763c3c8e308f2b
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-02-12 21:23:21 +0800

    move things to plugin and add <a> to names

--- page-about.php
+++ page-about.php
@@ -2,41 +2,9 @@
   // Template Name: Quienes Somos
   get_header();
 
-  $usersIDs = get_users( array(
-    'orderby' => 'ID',
-    'fields' => array('ID'),
-    'who' => 'authors'
-  ) );
-
-  $groups = array(
-    'principal' => '1',
-    'colaborators' => '2'
-  );
-
-  $authors = [];
-  foreach ($usersIDs as $id => $user) {
-    $group = get_the_author_meta( '_moebius_user_group', $user->ID );
-
-    if ( !empty($group) ) {
-      if ( array_key_exists($group, $authors) ) {
-        $authors[$group][] = get_authors_meta_info($user->ID);
-      } else {
-        $authors[$group] = [];
-        $authors[$group][] = get_authors_meta_info($user->ID);
-      }
-    }
-  }
-
-  function get_authors_meta_info($userID) {
-    $author = [];
-    $author['ID']         = get_the_author_meta( 'ID', $userID );
-    $author['bio']        = get_the_author_meta( '_moebius_user_bio', $userID );
-    $author['img']        = get_the_author_meta( '_moebius_user_img_id', $userID );
-    $author['role_title'] = get_the_author_meta( '_moebius_user_role_title', $userID );
-    $author['name']       = get_the_author_meta( 'display_name', $userID );
-    $author['url']        = get_the_author_meta( 'user_url', $userID );
-    return $author;
-  }
+  $users = get_moebius_users();
+  $authors = $users['authors'];
+  $groups = $users['groups'];
 
   function build_authors_group($group) {
     $html = '';
@@ -44,7 +12,7 @@
       $html .= '<div class="author-section">';
         $html .= '<div class="author-img m-all t-1of3 d-1of3 ld-1of4">' . wp_get_attachment_image($author['img'], 'moebius-square') . '</div>';
         $html .= '<div class="author-info m-all t-2of3 d-2of3 ld-3of4">';
-          $html .= '<h3 class="author-name">' . $author['name'] . '</h3>';
+          $html .= '<h3 class="author-name"><a href="/?s=' . urlencode( remove_accents($author['name']) ) . '">' . $author['name'] . '</a></h3>';
           $html .= '<p class="author-role">' . $author['role_title'] . '</p>';
           $html .= '<div class="author-bio">' . apply_filters( 'the_content', $author['bio'] ) . '</div>';
         $html .= '</div>';
